6a94f2f5dfe6d1f39bebfc6501e837f66c233c61
unknown <gatly0145@gmail.com>
2020-09-09 02:59:08 -0700

    Updates

---
+++ precache-manifest.1a4ef9b6d3dbbd64e1a956b57766c39c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef20313cb63e82f01ee9d896cfd3a043",
+    "url": "/tesla/index.html"
+  },
+  {
+    "revision": "a92281c0a8b4935f3d2d",
+    "url": "/tesla/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "8e73bf49a9fca0d4b117",
+    "url": "/tesla/static/css/main.5610237b.chunk.css"
+  },
+  {
+    "revision": "a92281c0a8b4935f3d2d",
+    "url": "/tesla/static/js/2.8c97dd8c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/tesla/static/js/2.8c97dd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e73bf49a9fca0d4b117",
+    "url": "/tesla/static/js/main.3d1c8cc7.chunk.js"
+  },
+  {
+    "revision": "16a3fdaff4701ee5d515",
+    "url": "/tesla/static/js/runtime-main.d7ad9925.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tesla/static/media/logo.5d5d9eef.svg"
+  }
+]);
